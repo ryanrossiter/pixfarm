bc704d1eac08cea3311ab84066b76dd40722ea76
Ryan Rossiter <nayr0788@gmail.com>
2018-02-22 17:19:24 -0500

    Add basic setup, gotta stop tho cause I'm getting a stupid memory leak - I think due to my laptop, needs testing

---
+++ src/defs.js
@@ -0,0 +1,20 @@
+export default {
+    PIXEL_SIZE: 6,
+    TILE_SIZE: 4,
+
+    PIXEL_SPRITES: {
+        'smile': [
+            '..F...F..',
+            '.........',
+            '....F....',
+            'F.......F',
+            '.FFFFFFF.',
+        ],
+        'ground': [
+            '6666',
+            '5C55',
+            '55C5',
+            'C555',
+        ]
+    }
+};
